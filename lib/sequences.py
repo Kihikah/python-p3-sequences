d3611f828b19c12d31170cf9abef99fd7d34e38b
Kariuki Kihikah <kariukikihikah@gmail.com>
2025-05-06 13:02:13 +0300

    Fib

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+
+    if length > 0:
+        fib_sequence = [0]
+    if length > 1:
+        fib_sequence.append(1)
+    while len(fib_sequence) < length:
+        next_value = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_value)
+
+    print(fib_sequence)
+
